c5f7010d4424b7916cbe5c9b0bc7a55efd7d9fa7
samapriya <samapriya.roy@gmail.com>
2019-11-12 09:34:34 -0500

    updated v0.0.1
    * Major revision to overall tool and general improvements
    * Updated methodology to get private and public asset list.
    * Now supports export of table apart from image and image collection.
    * Asset search improved to include tags, provider and id path.

--- gee2drive/bandtypes.py
+++ gee2drive/bandtypes.py
@@ -4,96 +4,92 @@ import ee
 import os
 import time
 import re
+import sys
+
 try:
     ee.Initialize()
-except Exception, e:
-    print 'Authenticate Earth Engine first and rerun program'
-    time.sleep(2)
-    os.system('earthengine authenticate')
+except Exception as e:
+    sys.exit("Authenticate Earth Engine first and rerun program")
 
 
 ##great alphanumeric sorting https://stackoverflow.com/questions/2669059/how-to-sort-alpha-numeric-set-in-python
+
 
 def sorted_nicely(l):
     """ Sort the given iterable in the way that humans expect."""
 
     convert = lambda text: (int(text) if text.isdigit() else text)
-    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)'
-                                , key)]
+    alphanum_key = lambda key: [convert(c) for c in re.split("([0-9]+)", key)]
     return sorted(l, key=alphanum_key)
 
 
 def imgexp(collection):
-    typ = ee.data.getInfo(collection)['type']
-    print typ + ' ' + str(collection)
-    if typ == 'Image':
+    typ = ee.data.getInfo(collection)["type"]
+    print(typ + " " + str(collection))
+    if typ == "Image":
         info = ee.Image(collection).bandTypes().getInfo()
         b16uint = []
         b32uint = []
         b16sint = []
         b8uint = []
         fl = []
-        for (key, value) in \
-            ee.Image(collection).bandTypes().getInfo().items():
-            if value['precision'] == 'float':
+        for (key, value) in ee.Image(collection).bandTypes().getInfo().items():
+            if value["precision"] == "float":
                 fl.append(str(key))
-            elif value['precision'] == 'int':
-                if value['min'] == 0 and value['max'] == 255:
+            elif value["precision"] == "int":
+                if value["min"] == 0 and value["max"] == 255:
                     b8uint.append(str(key))
-                if value['min'] == -32768 and value['max'] == 32767:
+                if value["min"] == -32768 and value["max"] == 32767:
                     b16sint.append(str(key))
-                if value['min'] == 0 and value['max'] == 65535:
+                if value["min"] == 0 and value["max"] == 65535:
                     b16uint.append(str(key))
-                if value['min'] == 0 and value['max'] == 4294967295:
+                if value["min"] == 0 and value["max"] == 4294967295:
                     b32uint.append(str(key))
         if len(b8uint) > 0:
-            print '8 bit unsigned integer bands ' + str(b8uint)
+            print("8 bit unsigned integer bands " + str(b8uint))
         if len(b16uint) > 0:
-            print '16 bit unsigned integer bands ' \
-                + str(sorted_nicely(b16uint))
+            print("16 bit unsigned integer bands " + str(sorted_nicely(b16uint)))
         if len(b16sint) > 0:
-            print '16 bit signed integer bands ' \
-                + str(sorted_nicely(b16sint))
+            print("16 bit signed integer bands " + str(sorted_nicely(b16sint)))
         if len(b32uint) > 0:
-            print '32 bit unsigned integer bands ' \
-                + str(sorted_nicely(b32uint))
+            print("32 bit unsigned integer bands " + str(sorted_nicely(b32uint)))
         if len(fl) > 0:
-            print 'Bands of type Float ' + str(fl)
-    elif typ == 'ImageCollection':
-        info = \
-            ee.Image(ee.ImageCollection(collection).first()).getInfo()
+            print("Bands of type Float " + str(fl))
+    elif typ == "ImageCollection":
+        info = ee.Image(ee.ImageCollection(collection).first()).getInfo()
         b16uint = []
         b32uint = []
         b16sint = []
         b8uint = []
         fl = []
-        for (key, value) in \
-            ee.Image(ee.ImageCollection(collection).first()).bandTypes().getInfo().items():
-            if value['precision'] == 'float':
+        for (key, value) in (
+            ee.Image(ee.ImageCollection(collection).first())
+            .bandTypes()
+            .getInfo()
+            .items()
+        ):
+            if value["precision"] == "float":
                 fl.append(str(key))
-            elif value['precision'] == 'int':
+            elif value["precision"] == "int":
 
-                if value['min'] == 0 and value['max'] == 255:
+                if value["min"] == 0 and value["max"] == 255:
                     b8uint.append(str(key))
-                if value['min'] == -32768 and value['max'] == 32767:
+                if value["min"] == -32768 and value["max"] == 32767:
                     b16sint.append(str(key))
-                if value['min'] == 0 and value['max'] == 65535:
+                if value["min"] == 0 and value["max"] == 65535:
                     b16uint.append(str(key))
-                if value['min'] == 0 and value['max'] == 4294967295:
+                if value["min"] == 0 and value["max"] == 4294967295:
                     b32uint.append(str(key))
         if len(b8uint) > 0:
-            print '8 bit unsigned integer bands ' + str(b8uint)
+            print("8 bit unsigned integer bands " + str(b8uint))
         if len(b16uint) > 0:
-            print '16 bit unsigned integer bands ' \
-                + str(sorted_nicely(b16uint))
+            print("16 bit unsigned integer bands " + str(sorted_nicely(b16uint)))
         if len(b16sint) > 0:
-            print '16 bit signed integer bands ' \
-                + str(sorted_nicely(b16sint))
+            print("16 bit signed integer bands " + str(sorted_nicely(b16sint)))
         if len(b32uint) > 0:
-            print '32 bit unsigned integer bands ' \
-                + str(sorted_nicely(b32uint))
+            print("32 bit unsigned integer bands " + str(sorted_nicely(b32uint)))
         if len(fl) > 0:
-            print 'Bands of type Float ' + str(fl)
+            print("Bands of type Float " + str(fl))
 
 
 # imgexp(collection='USGS/SRTMGL1_003')
--- gee2drive/idsearch.py
+++ gee2drive/idsearch.py
@@ -2,23 +2,49 @@
 # -*- coding: utf-8 -*-
 import os
 import csv
+from prettytable import PrettyTable
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 src = os.path.dirname(os.path.realpath(__file__))
 
+x = PrettyTable()
+
 
 def idsearch(mname):
     for items in os.listdir(src):
-        if items.endswith('.csv'):
+        if items.endswith(".csv"):
+            i = 1
             input_file = csv.DictReader(open(os.path.join(src, items)))
             for rows in input_file:
-                if mname.upper().lower() in str(rows['title'
-                        ]).upper().lower():
+                if mname.lower() in str(rows["title"]).lower():
                     try:
-                        print str(rows['title']) + ' >> ' \
-                            + str(rows['id'])
-                    except Exception, e:
-                        print e
+                        x.field_names = ["index", "name", "id"]
+                        x.add_row([i, rows["title"], rows["id"]])
+                        i = i + 1
+                    except Exception as e:
+                        print(e)
+                elif mname.lower() in str(rows["id"]).lower():
+                    try:
+                        x.field_names = ["index", "name", "id"]
+                        x.add_row([i, rows["title"], rows["id"]])
+                        i = i + 1
+                    except Exception as e:
+                        print(e)
+                elif mname.lower() in str(rows["provider"]).lower():
+                    try:
+                        x.field_names = ["index", "name", "id"]
+                        x.add_row([i, rows["title"], rows["id"]])
+                        i = i + 1
+                    except Exception as e:
+                        print(e)
+                elif mname.lower() in str(rows["tags"]).lower():
+                    try:
+                        x.field_names = ["index", "name", "id"]
+                        x.add_row([i, rows["title"], rows["id"]])
+                        i = i + 1
+                    except Exception as e:
+                        print(e)
+    print(x)
 
 
 # idsearch(mname='Belem')
